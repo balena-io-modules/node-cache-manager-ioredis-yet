a24915ccd482dd0bfbc14d2f7f43c43a2e2d63f6
Juan Aguilar Santillana <mhpoin@gmail.com>
2022-08-04 00:43:08 +0200

    fix: improve types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { callbackify } from 'node:util';
-import { createClient, RedisClientOptions } from 'redis';
+import { createClient, RedisClientOptions, RedisClientType } from 'redis';
 import '@redis/client';
 import '@redis/bloom';
 import '@redis/graph';
@@ -7,16 +7,12 @@ import '@redis/json';
 import '@redis/search';
 import '@redis/time-series';
 
-export type WrapArgsType<T> =
-  | string
-  | ((callback: CB<T>) => void)
-  | CachingConfig
-  | CB<T>
-  | (() => PromiseLike<T> | T);
+import { Cache, Store } from 'cache-manager';
 
 type CachingConfig = {
   ttl?: number | ((value: unknown) => number);
 };
+
 type CB<T> = (err: NodeJS.ErrnoException | null, result: T | null) => void;
 
 type CBSet = (err: NodeJS.ErrnoException | null) => void;
@@ -26,19 +22,43 @@ export interface CacheManagerOptions {
   isCacheableValue?: (value: unknown) => boolean;
 }
 
-export type Awaited<T> = T extends Promise<infer U> ? U : never;
-export type RedisStore = Awaited<ReturnType<typeof redisStore>>;
-
-export interface RedisCache {
+export interface RedisCache extends Cache {
   store: RedisStore;
-  wrap<T>(...args: WrapArgsType<T>[]): Promise<T>;
   set: RedisStore['set'];
   get: RedisStore['get'];
   del: RedisStore['del'];
   reset: RedisStore['reset'];
 }
 
-export type RedisClientType = RedisStore['getClient'];
+export interface RedisStore extends Store {
+  name: 'redis';
+  isCacheableValue: (value: unknown) => boolean;
+  get getClient(): RedisClientType;
+  set<T>(key: string, value: T, options?: CachingConfig): Promise<void>;
+  set<T>(key: string, value: T, ttl: number): Promise<void>;
+  set<T>(key: string, value: T, options: CachingConfig, callback: CBSet): void;
+  set<T>(key: string, value: T, ttl: number, callback: CBSet): void;
+
+  get<T>(key: string, opt: null, callback: CB<T>): void;
+  get<T>(key: string): Promise<T | undefined>;
+
+  del(key: string, callback: CBSet): void;
+  del(key: string[], callback: CBSet): void;
+  del(key: string): Promise<void>;
+  del(key: string[]): Promise<void>;
+
+  reset(): Promise<void>;
+  reset(cb: () => void): void;
+
+  mset(args: [string, unknown][], ttl?: number): Promise<void>;
+
+  mget(...args: string[]): Promise<unknown[]>;
+
+  keys(pattern: string | undefined, cb: CB<string[]>): void;
+  keys(pattern?: string): Promise<string[]>;
+
+  ttl(key: string): Promise<number>;
+}
 
 const getVal = (value: unknown) => JSON.stringify(value) || '"undefined"';
 export async function redisStore(
@@ -70,21 +90,21 @@ export async function redisStore(
     if (ttl) await redisCache.setEx(key, ttl, getVal(value));
     else await redisCache.set(key, getVal(value));
   };
+  const reset = async () => {
+    await redisCache.flushDb();
+  };
+
   return {
     name: 'redis' as const,
     isCacheableValue,
     get getClient() {
       return redisCache;
     },
-    set: <T>(
-      key: string,
-      value: T,
-      ttl?: number | CachingConfig,
-      cb?: CBSet,
-    ) => {
+    set<T>(key: string, value: T, ttl?: number | CachingConfig, cb?: CBSet) {
       if (typeof cb === 'function') callbackify(set<T>)(key, value, ttl, cb);
       else return set<T>(key, value, ttl);
     },
+    // TODO: callbackify
     mset: async (args: [string, unknown][], ttl?: number) => {
       if (!ttl) ttl = options?.ttl;
 
@@ -105,30 +125,31 @@ export async function redisStore(
           }),
         );
     },
-    get: <T>(key: string, opt?: null, cb?: CB<T>) => {
+    get<T>(key: string, opt?: null, cb?: CB<T>) {
       if (typeof cb === 'function') callbackify(get<T>)(key, cb);
       else return get<T>(key);
     },
+    // TODO: callbackify
     mget: (...args: string[]) =>
       redisCache
         .mGet(args)
         .then((x) => x.map((x) => (x === null ? null : JSON.parse(x)))),
-    del: (args: string | string[], cb?: CBSet) => {
+    del(args: string | string[], cb?: CBSet) {
       const fn = async () => {
         await redisCache.del(args);
       };
       if (typeof cb === 'function') callbackify(fn)(cb);
       else return fn();
     },
-    reset: (cb?: CB<string>) => {
-      if (typeof cb === 'function') callbackify(redisCache.flushDb)(cb);
-      else return redisCache.flushDb();
+    reset(cb?: CBSet) {
+      if (typeof cb === 'function') callbackify(reset)(cb);
+      else return reset();
     },
-    keys: (pattern = '*', cb?: CB<string[]>) => {
+    keys(pattern = '*', cb?: CB<string[]>) {
       if (typeof cb === 'function')
         callbackify(() => redisCache.keys(pattern))(cb);
       else return redisCache.keys(pattern);
     },
     ttl: (key: string) => redisCache.ttl(key),
-  };
+  } as RedisStore;
 }
